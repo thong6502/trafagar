01140eff57c417d4a98e397a1907593a8a831650
LeVanThong <lvthong652002@gmail.com>
2023-02-18 22:46:08 +0700

    firts commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(".review-list").slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  prevArrow:
+    "<button type='button' class='slick-arrow slick-prev pull-left'><i class='fa-solid fa-arrow-left-long' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-arrow slick-next pull-right'><i class='fa-solid fa-arrow-right-long' aria-hidden='true'></i></button>",
+});
